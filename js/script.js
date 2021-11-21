cef192c49f72282fe1f767b3b2d8bfb18c0ee38c
IkkiRama <georgeikkirama@gmail.com>
2021-11-21 20:01:20 +0700

    header sudah responsive

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let navMobile = document.querySelector(".containerNavMobile")
+let menu = document.querySelector(".menu")
+menu.addEventListener("click", () => {
+    navMobile.classList.toggle("show")
+})
